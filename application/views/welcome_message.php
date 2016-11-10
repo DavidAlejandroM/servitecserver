dc2041bb72b85267448bdeb065661b6857e3c768
junmper <jandro240@gmail.com>
2016-11-10 17:43:18 -0500

    aplicativo web

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -1,49 +1,59 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
-?><!DOCTYPE html>
+?>
+<!DOCTYPE html>
 <html>
 <head>
+    <link href="css/style.css" rel="stylesheet">
+    <link href="css/materialize.min.css" rel="stylesheet">
+
     <!--Import Google Icon Font-->
     <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-    
-	<!--Import materialize.css-->
-    <link type="text/css" rel="stylesheet" href="lib/materialize/css/materialize.min.css" media="screen,projection"/>
-    <link type="text/css" rel="stylesheet" href="css/style.css" media="screen,projection"/>
 
     <!--Let browser know website is optimized for mobile-->
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-	<!--Import jQuery before materialize.js-->
-	<script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
-	<script type="text/javascript" src="lib/materialize/js/materialize.min.js"></script>
 
-	<!--JS Angular-->
-	<script type="text/javascript" src="lib/angular/angular.js"></script>
-	
-	<!--script-->
-	<script src="js/controllers/MapCtrl.js"></script>
+    <script src="js/lib/jquery.min.js"></script>
+    <script src="js/lib/materialize.min.js"></script>
+    <script src="js/lib/angular.min.js"></script>
+
+    <script src="js/controllers/MapCtrl.js"></script>
 </head>
-
-<body>
-
-
-
+<body ng-app="servitecWeb" ng-controller="MapCtrl">
+<div class="">
     <nav>
         <div class="nav-wrapper colorApp">
-            <ul class="left hide-on-med-and-down">
-                <li><a href="sass.html"><i class="material-icons">search</i></a></li>
-                <li><a href="badges.html"><i class="material-icons">view_module</i></a></li>
-                <li><a href="collapsible.html"><i class="material-icons">refresh</i></a></li>
-                <li><a href="mobile.html"><i class="material-icons">more_vert</i></a></li>
+            <a href="#!" class="brand-logo"><i class="material-icons">cloud</i>Logo</a>
+            <ul class="right hide-on-med-and-down">
+                <li><a href=""><i class="material-icons">search</i></a></li>
+                <li><a href=""><i class="material-icons">view_module</i></a></li>
+                <li><a href=""><i class="material-icons">refresh</i></a></li>
+                <li><a href=""><i class="material-icons">more_vert</i></a></li>
             </ul>
-            <a href="#!" class="right brand-logo"><i class="material-icons">cloud</i>Servitec</a>
-
         </div>
     </nav>
-	<div id="map" style="height: 100%; width: 100%;"></div>
-	
-<!--JS Google Map-->
-    <script async defer
-            src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBv7SeCNl6N6dYQLcmieAhenQ3WZ37XevU">
-    </script>
+    <div class="row">
+
+        <div class="col s8 z-depth-2" id="map">
+
+        </div>
+
+        <div class="col s4">
+            <div class="row valign-wrapper" style="background-color: #8ba8af">
+                <div class="col s3">
+                    <img ng-src="img/sr-05" style="width: 100%;">
+                </div>
+                <div class="col s9">hola</div>
+            </div>
+        </div>
+
+    </div>
+
+
+</div>
+
+<script async defer
+        src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBv7SeCNl6N6dYQLcmieAhenQ3WZ37XevU&callback">
+</script>
 </body>
 </html>
--- js/controllers/MapCtrl.js
+++ js/controllers/MapCtrl.js
@@ -1,18 +1,37 @@
 /**
  * Created by DEIRY on 8/11/2016.
  */
-var map;
 
-$( document ).ready(function() {
-    document.getElementById('map').style.position = 'static';
-    setTimeout(function () {
-        map = new google.maps.Map(document.getElementById('map'),{
-            zoom: 18,
-            center: {lat: 6.2518, lng: -75.5636},
-            disableDefaultUI: true,
-            scrollwheel: true
+
+angular.module('servitecWeb', [])
+    .controller('MapCtrl', function ($scope, $timeout)
+    {
+        var latLng;
+        var map;
+        angular.element(document).ready(function ()
+        {
+            var nav_size = $('nav').height();
+            var window_size = $(window).height();
+            $('#map').height(window_size - nav_size - 10);
+
+
+            latLng = new google.maps.LatLng({lat: $scope.lat, lng: $scope.lng});
+            $scope.mostrarMapa();
+
         });
-        console.log("hola");
-    }, 3000);
 
-});
+        $scope.mostrarMapa = function(){
+
+            $timeout(function(){
+                map = new google.maps.Map(document.getElementById('map'),{
+                    zoom: 15,
+                    center: {lat: 6.2518, lng: -75.5636},
+                    disableDefaultUI: true,
+                    scrollwheel: true
+                });
+            },3000);
+
+        };
+
+
+    });
